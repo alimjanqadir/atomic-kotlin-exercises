ae5fa398437a83174f87b4e6929ba63496f39b6e
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-07-04 15:34:09 +0800

    RepetitionWithWhile task 2 is done.

--- Programming Basics/Repetition with while/Exercise 2/src/Task.kt
+++ Programming Basics/Repetition with while/Exercise 2/src/Task.kt
@@ -2,7 +2,14 @@
 package repetitionWithWhileExercise2
 
 fun sum(number: Int): Int {
-  TODO()
+  var sum = 0
+  var i = 0
+  while(i <= number) {
+    sum += i
+    i += 1
+  }
+
+  return sum
 }
 
 fun main() {
