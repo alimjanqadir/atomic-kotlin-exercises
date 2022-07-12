3d212e042686e8e0dc60c81cf3d557d223dbc028
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-07-12 22:21:43 +0800

    Properties section is finished.

--- Introduction to Objects/Properties/Exercise 3/src/Task.kt
+++ Introduction to Objects/Properties/Exercise 3/src/Task.kt
@@ -4,21 +4,35 @@ package propertiesExercise3
 class Robot {
   var x = 0
   var y = 0
+  private val fieldSize = 100
+
+  private fun crossBoundary(location: Int): Int {
+    val bound = location % fieldSize
+    return if(bound < 0) {
+      fieldSize + bound
+    }else {
+      bound
+    }
+  }
 
   fun right(steps: Int) {
     x += steps
+    x = crossBoundary(x)
   }
 
   fun left(steps: Int) {
     x -= steps
+    x = crossBoundary(x)
   }
 
   fun down(steps: Int) {
     y += steps
+    y = crossBoundary(y)
   }
 
   fun up(steps: Int) {
     y -= steps
+    y = crossBoundary(y)
   }
 
   fun getLocation(): String = "($x, $y)"
