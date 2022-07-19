5bcf2dd336d440746a22aca3642b7e118d515977
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-07-19 23:16:58 +0800

    Testing section is finished.

--- Introduction to Objects/Testing/Exercise 2/src/Task.kt
+++ Introduction to Objects/Testing/Exercise 2/src/Task.kt
@@ -3,8 +3,13 @@ package testingExercise2
 import atomictest.eq
 import atomictest.neq
 
-fun sum(a: Int, b: Int, c: Int): Int = TODO()
+fun sum(a: Int, b: Int, c: Int): Int = a + b + c
 
 fun main() {
-  TODO()
+  sum(1, 2, 3) eq 6
+  sum(2, 3, 4) eq 9
+  sum(4, 5, 6) eq 15
+  sum(7, 8, 9) eq 24
+  sum(7, 8, 9) neq 25
+
 }
