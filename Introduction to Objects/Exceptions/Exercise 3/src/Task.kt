716d7430f2432a94047bc80355cfcac211390f3a
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-07-21 18:22:52 +0800

    Exceptions section is finished.

--- Introduction to Objects/Exceptions/Exercise 3/src/Task.kt
+++ Introduction to Objects/Exceptions/Exercise 3/src/Task.kt
@@ -4,7 +4,14 @@ import atomictest.capture
 import atomictest.eq
 
 fun repeatChar(ch: Char, n: Int): String {
-  TODO()
+  if(n < 0) {
+    throw IllegalArgumentException("Count 'n' must be non-negative, but was $n.")
+  }
+  var result = ""
+  repeat(n) {
+    result += ch
+  }
+  return result
 }
 
 fun main() {
