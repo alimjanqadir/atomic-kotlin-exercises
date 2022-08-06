acda7b61655be5e4a1671188315f1779ad89c691
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-08-06 21:51:03 +0800

    Destructuring Declarations section is finished.

--- Usability/Destructuring Declarations/Exercise 1/src/Task.kt
+++ Usability/Destructuring Declarations/Exercise 1/src/Task.kt
@@ -4,8 +4,12 @@ import atomictest.eq
 
 fun calculate(
   n1: Int, n2: Int
-): Any /* replace with required type */ {
-  TODO()
+): Triple<Boolean, Int, Int> {
+  if(n1 < 0 || n2 < 0) {
+    return Triple(false, 0, 0)
+  } else {
+    return Triple(true, n1 + n2, n1 * n2)
+  }
 }
 
 fun main() {
