393142c08626758fbfcb5da92af517e3efb915dd
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-08-13 00:11:16 +0800

    Usability Module is finished.

--- Usability/break   continue/Exercise 1/src/Task.kt
+++ Usability/break   continue/Exercise 1/src/Task.kt
@@ -3,12 +3,25 @@ package breakAndContinueExercise1
 import atomictest.*
 
 fun readNumbers(vararg n: String) {
-  TODO()
+    var sum = 0
+    for(s in n) {
+        when (val number = s.toIntOrNull()) {
+            0 -> {
+                trace("Sum: $sum")
+                break
+            }
+            null -> {
+                trace("Not a number: $s")
+                continue
+            }
+            else -> sum += number
+        }
+    }
 }
 
 fun main() {
-  readNumbers("1", "a", "3", "10", "0", "19")
-  trace eq """
+    readNumbers("1", "a", "3", "10", "0", "19")
+    trace eq """
     Not a number: a
     Sum: 14
   """
