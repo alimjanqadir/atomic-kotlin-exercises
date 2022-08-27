49c590d715af96b95859cab5da494867fa249c8d
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-08-27 21:47:48 +0800

    Functional Programming module is finished.

--- Functional Programming/Recursion/Exercise 2/src/Task.kt
+++ Functional Programming/Recursion/Exercise 2/src/Task.kt
@@ -1,13 +1,20 @@
 // Recursion/Task2.kt
 package recursionExercise2
+
 import atomictest.eq
 
 fun factorial(n: Long): Long {
-  TODO()
+    tailrec fun factorial(
+            n: Long,
+            acc: Long
+    ): Long =
+            if (n == 1L) acc
+            else factorial(n - 1, acc * n)
+    return factorial(n, 1)
 }
 
 fun main() {
-  factorial(3) eq 6
-  factorial(4) eq 24
-  factorial(11) eq 39916800
+    factorial(3) eq 6
+    factorial(4) eq 24
+    factorial(11) eq 39916800
 }
