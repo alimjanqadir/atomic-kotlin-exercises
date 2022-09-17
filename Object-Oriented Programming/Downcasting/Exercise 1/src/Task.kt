d7e339c3006fdb02b61ae221af2e0b800f020629
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-09-17 22:24:09 +0800

    Downcasting section exercise 1 is finished.

--- Object-Oriented Programming/Downcasting/Exercise 1/src/Task.kt
+++ Object-Oriented Programming/Downcasting/Exercise 1/src/Task.kt
@@ -1,29 +1,48 @@
 // DownCasting/DownCastEx1.kt
 package downcastingExercise1
+
 import atomictest.*
 
 interface Base {
-  fun f()
+    fun f()
 }
 
 class Derived1 : Base {
-  override fun f() { TODO() }
-  fun g() { TODO() }
+    override fun f() {
+        trace("Derived1.f()")
+    }
+
+    fun g() {
+        trace("Derived1.g()")
+    }
 }
 
 class Derived2 : Base {
-  override fun f() { TODO() }
-  fun h() { TODO() }
+    override fun f() {
+        trace("Derived2.f()")
+    }
+
+    fun h() {
+        trace("Derived2.h()")
+    }
 }
 
 fun checkAndCall(b: Base) {
-  TODO()
+    b.f()
+    when (b) {
+        is Derived1 -> {
+            b.g()
+        }
+        is Derived2 -> {
+            b.h()
+        }
+    }
 }
 
 fun main() {
-  checkAndCall(Derived1()) // Upcast
-  checkAndCall(Derived2()) // Upcast
-  trace eq """
+    checkAndCall(Derived1()) // Upcast
+    checkAndCall(Derived2()) // Upcast
+    trace eq """
     Derived1.f()
     Derived1.g()
     Derived2.f()
