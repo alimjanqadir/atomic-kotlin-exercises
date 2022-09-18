2654fc137d6da788c40462226a04b8db22fe3ccc
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-09-18 21:24:08 +0800

    Class Delegation exercise 2 is finished.

--- Object-Oriented Programming/Downcasting/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Downcasting/Exercise 2/src/Task.kt
@@ -3,4 +3,15 @@ package downcastingExercise2
 import downcasting.*
 import atomictest.eq
 
-// TODO
+val group: List<Creature> = listOf(
+        Human(), Human(), Dog(), Alien(), Dog()
+)
+
+fun main() {
+    downcasting.group
+            .filterIsInstance<Dog>()
+            .map(Dog::bark)
+    downcasting.group
+            .filterIsInstance(Dog::class.java)
+            .map(Dog::bark)
+}
