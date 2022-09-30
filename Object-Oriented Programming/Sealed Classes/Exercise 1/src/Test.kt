f89af8bfda8c226ad859c8df12111141ddb1b2b3
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-09-30 22:39:04 +0800

    Sealed Classes exercise 1 is finished.

--- Object-Oriented Programming/Sealed Classes/Exercise 1/src/Test.kt
+++ Object-Oriented Programming/Sealed Classes/Exercise 1/src/Test.kt
@@ -8,15 +8,3 @@ sealed interface UiStateWrapper {
     class Failure(val exception: Throwable) : UiStateWrapper
 }
 
-# DO NOT
-- Project
-- ui
-- feature1
-- feature2
-- feature3
-- domain
-- feature1
-- feature2
-- feature3
-- data
-
