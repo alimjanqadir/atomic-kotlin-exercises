2b106bd7231cf9742dfc65d291d4608b6806df3a
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-10-13 23:06:36 +0800

    Inner Classes exercise 2 is completed.

--- Object-Oriented Programming/Inner Classes/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Inner Classes/Exercise 2/src/Task.kt
@@ -1,14 +1,24 @@
 // InnerClasses/InnerEx2.kt
 package innerClassesExercise2
+
 import atomictest.eq
 
 class Box(contains: String, label: String) {
-  // TODO
-  fun manifest() =
-    "TODO"
+    inner class Contents(val contains: String)
+    inner class Destination(val label: String)
+
+    private val contents = Contents(contains)
+    private val destination = Destination(label)
+
+    private val x = object : Any() {
+        fun f() = "Any-based"
+    }
+
+    fun manifest() =
+            "${contents.contains}: ${destination.label} ${x.f()}"
 }
 
 fun main() {
-  Box("Computer", "Tasmania").manifest() eq
-    "Computer: Tasmania Any-based"
+    Box("Computer", "Tasmania").manifest() eq
+            "Computer: Tasmania Any-based"
 }
