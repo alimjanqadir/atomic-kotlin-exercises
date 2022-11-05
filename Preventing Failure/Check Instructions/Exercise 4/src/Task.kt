a3b7141282b7a447ea4d6f92d4a18d9f29f9705c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-05 16:19:10 +0800

    Check Instructions section is finished.

--- Preventing Failure/Check Instructions/Exercise 4/src/Task.kt
+++ Preventing Failure/Check Instructions/Exercise 4/src/Task.kt
@@ -1,8 +1,54 @@
 // CheckInstructions/CheckInstructionsSoln4.kt
 package checkInstructionsExercise4
+
 import atomictest.*
 
-class Tank
+class Tank(val capacity: Double) {
+    var level: Double = 0.0
+    val safetyGap = 0.02 * capacity
+    val maxLevel: Double = capacity - safetyGap
+
+    fun add(quantity: Double): Double {
+        require(0 < quantity) {
+            "quantity must be > 0, is $quantity"
+        }
+
+        var remainder = 0.0
+        val total = level + quantity
+        if (total <= maxLevel) {
+            level += quantity
+        } else {
+            remainder = total - maxLevel
+            level += maxLevel
+        }
+        check(level > 0) {
+            "Incorrect level: $e"
+        }
+
+        check(level <= maxLevel) {
+            "Safety level exceeded: $maxLevel"
+        }
+
+        check(remainder >= 0.0) {
+            "Remainder is negative: $remainder"
+        }
+        return remainder
+    }
+
+    fun drain(quantity: Double) {
+        require(0 < quantity) {
+            "quantity must be > 0, is $quantity"
+        }
+        require(quantity <= level) {
+            "rain exceeds tank amount: $quantity"
+        }
+        level -= quantity
+        check(level >= 0.0) {
+            "drain exceeds tank amount: $level"
+        }
+    }
+
+}
 
 fun main() {
 /*
