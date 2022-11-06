39d5385213cfec280c284ae657e884ab12841573
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-06 23:04:53 +0800

    Resource Cleanup exercise 1 is finished.

--- Preventing Failure/Resource Cleanup/Exercise 1/src/Task.kt
+++ Preventing Failure/Resource Cleanup/Exercise 1/src/Task.kt
@@ -1,7 +1,11 @@
 // ResourceCleanup/ResourceCleanupSoln1.kt
 package resourceCleanupExercise1
+
 import atomictest.trace
+import java.io.BufferedWriter
 import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
 
 val text = """
 Swing low, sweet chariot
@@ -9,12 +13,19 @@ Coming for to carry me home
 """.trim()
 
 fun writeAndRead() {
-  TODO()
+    val file = File("DataFiles/CleanupSoln1.txt")
+    if (file.exists()) {
+        file.delete()
+    }
+    file.createNewFile()
+    file.appendText(text)
+    trace(file.useLines { it.joinToString("\n") })
+    file.forEachLine { trace(it) }
 }
 
 fun main() {
-  writeAndRead()
-  trace eq """
+    writeAndRead()
+    trace eq """
     Swing low, sweet chariot
     Coming for to carry me home
     Swing low, sweet chariot
