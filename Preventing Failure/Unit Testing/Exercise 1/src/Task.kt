e22c1178b2bcc72f262ff8ba841bb5e4bfbf3511
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-07 18:08:54 +0800

    Unit Testting section exercise 1 is finished.

--- Preventing Failure/Unit Testing/Exercise 1/src/Task.kt
+++ Preventing Failure/Unit Testing/Exercise 1/src/Task.kt
@@ -1,8 +1,22 @@
 // Tests/unittesting/UnitTestingSoln1.kt
 package unitTestingExercise1
+
+import atomictest.eq
 import sealedclasses.Bus
 import sealedclasses.Train
 import sealedclasses.travel
 import kotlin.test.*
 
-// TODO
+
+@Test
+fun `train travel`() {
+    val result = travel(Train("S1"))
+    assertEquals("Train S1", result)
+}
+
+@Test
+fun `bus travel`() {
+    val result = travel(Bus("11", 90))
+    assertEquals("Bus 11: size 90", result)
+}
+
