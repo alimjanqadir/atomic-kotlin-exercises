8adb6a21c15fb82ec8db1bfc9f2991263480f501
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-07 19:18:59 +0800

    Unit Testing section exercise 2 is finished.

--- Preventing Failure/Unit Testing/Exercise 2/src/Task.kt
+++ Preventing Failure/Unit Testing/Exercise 2/src/Task.kt
@@ -5,4 +5,38 @@ import interfaces.DeepThought
 import interfaces.Quantum
 import kotlin.test.*
 
-// TODO
+class DesktopTest {
+    val desktop = Desktop()
+    @Test
+    fun prompt() {
+        assertEquals("Hello!", desktop.prompt())
+    }
+    @Test
+    fun calculateAnswer() {
+        assertEquals(11, desktop.calculateAnswer())
+    }
+}
+
+class DeepThoughtTest {
+    val deepThought = DeepThought()
+    @Test
+    fun prompt() {
+        assertEquals("Thinking...", deepThought.prompt())
+    }
+    @Test
+    fun calculateAnswer() {
+        assertEquals(42, deepThought.calculateAnswer())
+    }
+}
+
+class QuantumTest {
+    val quantum = Quantum()
+    @Test
+    fun prompt() {
+        assertEquals("Probably...", quantum.prompt())
+    }
+    @Test
+    fun calculateAnswer() {
+        assertEquals(-1, quantum.calculateAnswer())
+    }
+}
