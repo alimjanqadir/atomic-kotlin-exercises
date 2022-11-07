3a4b7c609846aabdfca2dba30f201ca2dfbd8278
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-07 16:26:53 +0800

    Resource Cleanup section is finished.

--- Preventing Failure/Resource Cleanup/Exercise 3/src/Task.kt
+++ Preventing Failure/Resource Cleanup/Exercise 3/src/Task.kt
@@ -1,27 +1,36 @@
 // ResourceCleanup/ResourceCleanupSoln3.kt
 package resourceCleanupExercise3
+
 import atomictest.*
 
 class Cleanup : AutoCloseable {
-  fun f() = trace("f()")
-  fun g() = trace("g()")
-  fun h() = trace("h()")
-  override fun close() = trace("close()")
+    fun f() = trace("f()")
+    fun g() = trace("g()")
+    fun h() = trace("h()")
+    override fun close() = trace("close()")
 }
 
 enum class Option { Normal, Return, Throw }
 
 fun verifyClose(opt: Option) {
-  TODO()
+    Cleanup().use {
+        it.f()
+        when (opt) {
+            Option.Normal -> it.g()
+            Option.Return -> return
+            Option.Throw -> throw Exception()
+        }
+        it.h()
+    }
 }
 
 fun main() {
-  verifyClose(Option.Normal)
-  verifyClose(Option.Return)
-  capture {
-    verifyClose(Option.Throw)
-  } eq "Exception"
-  trace eq """
+    verifyClose(Option.Normal)
+    verifyClose(Option.Return)
+    capture {
+        verifyClose(Option.Throw)
+    } eq "Exception"
+    trace eq """
     f()
     g()
     h()
