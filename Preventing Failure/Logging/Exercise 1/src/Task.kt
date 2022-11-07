19695e48869782ba4ebd75959c43b157c64e0327
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-07 17:28:05 +0800

    Logging exercise 1 is finished.

--- Preventing Failure/Logging/Exercise 1/src/Task.kt
+++ Preventing Failure/Logging/Exercise 1/src/Task.kt
@@ -1,38 +1,55 @@
 // Logging/LoggingSoln1.kt
 package loggingExercise1
+
 import atomictest.*
 import atomiclog.Logger
 
 class Level(
-  val range: IntRange,
-  private var level: Int = range.start
+        val range: IntRange,
+        private var level: Int = range.start
 ) {
-  init {
-    require(level in range) {
-      "Initial level out of range: $level"
+    init {
+        require(level in range) {
+            "Initial level out of range: $level"
+        }
     }
-  }
-  val value: Int
-    get() = level
-  fun up(): Int {
-    if (level + 1 in range)
-      level++
-    return level
-  }
-  fun down(): Int {
-    if (level - 1 in range)
-      level--
-    return level
-  }
-  fun set(new: Int) {
-    require(new in range) {
-      "set() argument out of range: $new"
+
+    val value: Int
+        get() = level
+
+    fun up(): Int {
+        if (level + 1 in range)
+            level++
+        return level
     }
-    level = new
-  }
+
+    fun down(): Int {
+        if (level - 1 in range)
+            level--
+        return level
+    }
+
+    fun set(new: Int) {
+        require(new in range) {
+            "set() argument out of range: $new"
+        }
+        level = new
+    }
 }
 
-class Pipe
+class Pipe(val level: Level = Level(0..10)) {
+    val logger = Logger("PipeLog.txt")
+
+    fun checkLevel() {
+        when (level.value) {
+            2 -> trace("Level 1")
+            4 -> logger.debug("Level 4")
+            6 -> logger.info("Level 6")
+            8 -> logger.warn("Level 8")
+            10 -> logger.error("Level 10")
+        }
+    }
+}
 
 fun main() {
 /*
