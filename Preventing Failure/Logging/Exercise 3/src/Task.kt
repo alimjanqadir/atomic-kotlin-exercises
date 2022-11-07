978ac7126c7103cf0f5e1056bc434a99fe5eb506
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-07 17:49:50 +0800

    Logging section is finished.

--- Preventing Failure/Logging/Exercise 3/src/Task.kt
+++ Preventing Failure/Logging/Exercise 3/src/Task.kt
@@ -1,42 +1,45 @@
 // Logging/LoggingSoln3.kt
 package loggingExercise3
+
 import atomictest.eq
 import checkinstructions.DataFile
 import loggingExercise3.Level.*
 
 enum class Level {
-  // TODO: add values
+    Trace, Debug, Info, Warn, Error
 }
 
-var debugLevel: Level = TODO()
+var debugLevel: Level = Error
 
 class Logger(fileName: String) {
-  val logFile = DataFile(fileName).reset()
-  private fun log(type: String, msg: String) =
-    logFile.appendText("$type: $msg\n")
-  fun trace(msg: String) = log("Trace", msg)
-  fun debug(msg: String) = log("Debug", msg)
-  fun info(msg: String) = log("Info", msg)
-  fun warn(msg: String) = log("Warn", msg)
-  fun error(msg: String) = log("Error", msg)
-  fun report(msg: String) {
-    trace(msg)
-    debug(msg)
-    info(msg)
-    warn(msg)
-    error(msg)
-  }
+    val logFile = DataFile(fileName).reset()
+    private fun log(thisLevel: Level, msg: String) =
+
+            logFile.appendText("$thisLevel: $msg\n")
+
+    fun trace(msg: String) = log(Level.Trace, msg)
+    fun debug(msg: String) = log(Level.Debug, msg)
+    fun info(msg: String) = log(Level.Info, msg)
+    fun warn(msg: String) = log(Level.Warn, msg)
+    fun error(msg: String) = log(Level.Error, msg)
+    fun report(msg: String) {
+        trace(msg)
+        debug(msg)
+        info(msg)
+        warn(msg)
+        error(msg)
+    }
 }
 
 private val logger = Logger("LoggingSoln3.txt")
 
 fun main() {
-  Level.values().toList().forEach {
-    debugLevel = it
-    logger.report("debugLevel($debugLevel)")
-  }
-  logger.logFile.readText() eq
-  """
+    Level.values().toList().forEach {
+        debugLevel = it
+        logger.report("debugLevel($debugLevel)")
+    }
+    logger.logFile.readText() eq
+            """
   Trace: debugLevel(Trace)
   Debug: debugLevel(Trace)
   Info: debugLevel(Trace)
