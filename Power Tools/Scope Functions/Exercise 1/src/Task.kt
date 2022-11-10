f3d54e070d03a2cd91f7d7c2e1ecc707da6a6b92
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-10 16:07:43 +0800

    Scope Function section exercise 1 is finished.

--- Power Tools/Scope Functions/Exercise 1/src/Task.kt
+++ Power Tools/Scope Functions/Exercise 1/src/Task.kt
@@ -1,13 +1,36 @@
 // ScopeFunctions/Task1.kt
 package scopeFunctionsExercise1
+
 import atomictest.eq
 
 data class NumPair(var x: Int, var y: Int) {
-  fun add() = x + y
-  fun subtract() = x - y
-  fun multiply() = x * y
+    fun add() = x + y
+    fun subtract() = x - y
+    fun multiply() = x * y
 }
 
 fun main() {
-  // TODO
+    val np1 = NumPair(10, 20)
+    np1.apply {
+        x += 5
+        y += 6
+    } eq NumPair(15, 26)
+
+    val np2 = NumPair(30, 40)
+    np2.also {
+        it.x += 7
+        it.y += 8
+    } eq NumPair(37, 48)
+
+    val result1 = np1.run {
+        add() + subtract() + multiply()
+    } eq 420
+
+    val result2 = np1.let {
+        it.add() + it.subtract() + it.multiply()
+    } eq 420
+
+    val result3 = with(np1) {
+        add() + subtract() + multiply()
+    } eq 420
 }
