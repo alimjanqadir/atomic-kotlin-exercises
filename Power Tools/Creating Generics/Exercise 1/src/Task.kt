e35fc585bc3b24034ddfdf8114693565b36a9ff6
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-11 17:37:21 +0800

    Creating Generics section exercise 1 is finished.

--- Power Tools/Creating Generics/Exercise 1/src/Task.kt
+++ Power Tools/Creating Generics/Exercise 1/src/Task.kt
@@ -1,15 +1,27 @@
 // CreatingGenerics/CreatingGenericsSoln1.kt
 package creatingGenericsExercise1
+
 import atomictest.eq
 
-// TODO
+fun interface Items<T> {
+    fun next(): T?
+}
+
+fun <T> itemIter(vararg items: T): Items<T> {
+    var index = 0
+    return Items {
+        if (index < items.size) {
+            items[index++]
+        } else {
+            null
+        }
+    }
+}
 
 fun main() {
-/*
   val s = itemIter("A", "B", "C")
   (0..3).map { s.next() } eq "[A, B, C, null]"
   val i = itemIter(1, 2, 3, 4, 5, 6, 7)
   (0..10).mapNotNull { i.next() } eq
     "[1, 2, 3, 4, 5, 6, 7]"
-*/
 }
