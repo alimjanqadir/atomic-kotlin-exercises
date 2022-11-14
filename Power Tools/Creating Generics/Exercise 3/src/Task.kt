f96f9ae85c9908947069475113924ca63f7153c4
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-14 16:27:19 +0800

    Creating Generics section is completed.

--- Power Tools/Creating Generics/Exercise 3/src/Task.kt
+++ Power Tools/Creating Generics/Exercise 3/src/Task.kt
@@ -1,16 +1,30 @@
 // CreatingGenerics/CreatingGenericsSoln3.kt
 package creatingGenericsExercise3
+
 import variance.Cat
 
-// TODO
+interface InBox<in T> {
+    fun put(item: T)
+}
+
+interface OutBox<out T> {
+    fun get(): T
+}
+
+class Box<T>(private var item: T) : InBox<T>, OutBox<T> {
+    override fun put(item: T) {
+        this.item = item
+    }
+
+    override fun get(): T = item
+
+}
 
 fun main() {
-/*
-  val catBox: Box<Cat> = Box(Cat())
-  val outBoxAny: OutBox<Any> = catBox
-  val a: Any = outBoxAny.get()
-  val inBoxAny: InBox<Any> = Box(217)
-  val inBoxCat: InBox<Any> = inBoxAny
-  inBoxCat.put(Cat())
-*/
+    val catBox: Box<Cat> = Box(Cat())
+    val outBoxAny: OutBox<Any> = catBox
+    val a: Any = outBoxAny.get()
+    val inBoxAny: InBox<Any> = Box(217)
+    val inBoxCat: InBox<Any> = inBoxAny
+    inBoxCat.put(Cat())
 }
