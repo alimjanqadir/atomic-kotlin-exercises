c17d52146712d456a9f9ba61ecc3517975931ed0
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-14 19:13:27 +0800

    Operator Overloading exercise 1 is finished.

--- Power Tools/Operator Overloading/Exercise 1/src/Task.kt
+++ Power Tools/Operator Overloading/Exercise 1/src/Task.kt
@@ -1,42 +1,43 @@
 // OperatorOverloading/OpOverloadingSoln1.kt
 package operatorOverloadingExercise1
+
 import atomictest.*
 
-class WrapRange(
-  val range: IntRange,
-  private var current: Int = range.first
-) {
-  init {
-    // TODO
-  }
-  override fun toString() = "$current"
-  operator fun inc(): WrapRange {
-    TODO()
-  }
-  operator fun dec(): WrapRange {
-    TODO()
-  }
+class WrapRange(val range: IntRange, private var current: Int = range.first) {
+    init {
+        require(current < range.last) {
+            "'current' out of range: ${range.last}"
+        }
+    }
+
+    override fun toString() = "$current"
+    operator fun inc(): WrapRange {
+        current = if (current == range.last) range.first else current + 1
+        return this
+    }
+
+
+    operator fun dec(): WrapRange {
+        current = if (current == range.first) range.last else current - 1
+        return this
+    }
 }
 
 fun main() {
-  val range = 2..5
-  var wr = WrapRange(range)
-  trace(wr)
-  repeat(5) {
-    wr++
+    val range = 2..5
+    var wr = WrapRange(range)
     trace(wr)
-  }
-  trace("-")
-  repeat(2) {
-    wr--
-    trace(wr)
-  }
-  trace eq "2 3 4 5 2 3 - 2 5"
+    repeat(5) {
+        wr++
+        trace(wr)
+    }
+    trace("-")
+    repeat(2) {
+        wr--
+        trace(wr)
+    }
+    trace eq "2 3 4 5 2 3 - 2 5"
 
-  capture { WrapRange(range, 6) } eq
-    "IllegalArgumentException: " +
-    "'current' out of range: 6"
-  capture { WrapRange(range, -1) } eq
-    "IllegalArgumentException: " +
-    "'current' out of range: -1"
+    capture { WrapRange(range, 6) } eq "IllegalArgumentException: " + "'current' out of range: 6"
+    capture { WrapRange(range, -1) } eq "IllegalArgumentException: " + "'current' out of range: -1"
 }
