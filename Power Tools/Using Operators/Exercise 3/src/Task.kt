0b47391dbc662cdb6671422a226804a4f8dc2906
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-16 16:32:13 +0800

    Using Operators section is finished.

--- Power Tools/Using Operators/Exercise 3/src/Task.kt
+++ Power Tools/Using Operators/Exercise 3/src/Task.kt
@@ -1,14 +1,29 @@
 // UsingOperators/UsingOperatorsSoln3.kt
 package usingOperatorsExercise3
+
 import atomictest.trace
 import kotlin.random.Random
 
-data class N(var n: Int)
+data class N(var n: Int) : Comparable<N> {
+    override fun compareTo(other: N): Int = n.compareTo(other.n)
+
+    override fun toString() = "N($n)"
+}
 
 fun main() {
-  val rnd = Random(47) // For repeatability
-  TODO()
-  trace eq """
+    val rnd = Random(47) // For repeatability
+    val key = N(11)
+    val map = mutableMapOf(key to 47)
+    trace(map[key])
+    key.n++
+    trace(map[key])
+    val list = List(5) { N(it) }.shuffled(rnd)
+    trace("list: $list")
+    val sortedSet = list.toSortedSet()
+    trace("sorted set (1): $sortedSet")
+    sortedSet.minOrNull()!!.n += 2
+    trace("sorted set (2): $sortedSet")
+    trace eq """
     47
     null
     list: [N(4), N(0), N(2), N(3), N(1)]
