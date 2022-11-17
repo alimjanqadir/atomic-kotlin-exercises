b82ead60834efc6da7a2b8053e31a1237bf5628d
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-17 16:32:45 +0800

    Property Delegation exercise 2 is finished.

--- Power Tools/Property Delegation/Exercise 2/src/Task.kt
+++ Power Tools/Property Delegation/Exercise 2/src/Task.kt
@@ -1,17 +1,22 @@
 // PropertyDelegation/PropDelegationSoln3.kt
 package propertyDelegationExercise2
+
 import atomictest.eq
 import kotlin.reflect.KProperty
 
 class Delegator {
-  private var list = List(8) { "$it" }
-  var strings: List<String> = mutableListOf() // replace with 'by list'
-  // TODO getValue and setValue extensions
+    var strings: List<String> by List(8) { "$it" } // replace with 'by list'
 }
 
+operator fun List<String>.setValue(thisRef: Delegator, property: KProperty<*>, value: List<String>) {
+    thisRef.strings = value
+}
+
+operator fun List<String>.getValue(thisRef: Delegator, property: KProperty<*>): List<String> = thisRef.strings
+
 fun main() {
-  val x = Delegator()
-  x.strings eq "[0, 1, 2, 3, 4, 5, 6, 7]"
-  x.strings = listOf("99", "17")
-  x.strings eq "[99, 17]"
+    val x = Delegator()
+    x.strings eq "[0, 1, 2, 3, 4, 5, 6, 7]"
+    x.strings = listOf("99", "17")
+    x.strings eq "[99, 17]"
 }
