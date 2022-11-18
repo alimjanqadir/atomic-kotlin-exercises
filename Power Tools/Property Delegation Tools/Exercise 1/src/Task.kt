21e4f16ee5e0e8de3823053f4569d7d63bc3df92
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2022-11-18 14:30:18 +0800

    Property Delegation Tools section exercise 1 is finished.

--- Power Tools/Property Delegation Tools/Exercise 1/src/Task.kt
+++ Power Tools/Property Delegation Tools/Exercise 1/src/Task.kt
@@ -1,21 +1,27 @@
 // PropertyDelegation/PropDelegationSoln1.kt
 package propertyDelegationToolsExercise1
-import atomictest.eq
 
-class Configuration
+import atomictest.eq
+import propertydelegation.FileDelegate
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class Configuration(val map: MutableMap<String, Any>) {
+    var user by map
+    var id by map
+    var project by map
+}
 
 fun main() {
-/*
-  val config = Configuration(mutableMapOf(
-    "user" to "Luciano",
-    "id" to "Ramalho47",
-    "project" to "MyLittlePython",
-  ))
-  config.project eq "MyLittlePython"
-  config.user = "Crocubot"
-  config.id = "C137"
-  config.project = "WhirlyDirly"
-  config.map eq "{user=Crocubot, " +
-    "id=C137, project=WhirlyDirly}"
-*/
+    val config = Configuration(mutableMapOf(
+            "user" to "Luciano",
+            "id" to "Ramalho47",
+            "project" to "MyLittlePython",
+    ))
+    config.project eq "MyLittlePython"
+    config.user = "Crocubot"
+    config.id = "C137"
+    config.project = "WhirlyDirly"
+    config.map eq "{user=Crocubot, " +
+            "id=C137, project=WhirlyDirly}"
 }
